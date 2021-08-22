bb1daf545e08551bedad7e68808e479b93b5c065
kodai2610 <koudai_akiwa1230@keio.jp>
2021-08-22 18:41:07 +0900

    小数点処理完了

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var app = new Vue({
   data : {
     memory : null,
     func : null,
-    inputValue : 0, 
-    outputValue : 0,
+    inputValue : '0', //入力された値
+    outputValue : '0', //表示する値
     isError : false, //エラー時かどうか
   },
   methods : {
@@ -20,7 +20,7 @@ var app = new Vue({
       //データを初期化
       this.memory = null;
       this.func = null;
-      this.inputValue = 0;
+      this.inputValue = '0';
       this.outputValue = this.inputValue;
     },
     calc(memory,func,input) { //計算するメソッド ・演算子を入力したタイミングorイコールを押したタイミングでメモリに数値があれば計算
@@ -50,44 +50,45 @@ var app = new Vue({
       if(this.isError) {
         return;
       }
-      // if(number == '.' && this.outputValue.indexOf('.') < -1){
-      //   return;
-      // }
-
-      //0の時はただ変換するだけ
-      if(this.inputValue == 0) {
-        this.inputValue = number;
-        this.outputValue = this.inputValue;
-        return;
+      if(this.inputValue == '0') { //0の時はただ変換するだけ
+        if(number !== '.') {
+          this.inputValue = number;
+          this.outputValue = this.inputValue;
+          return;
+        }
       }
-      var parsed = parseInt(this.inputValue.toString() + number.toString()); //文字列の連結→整数にして29などを表現 
+      
+      var parsed = this.inputValue + number; //文字列の連結
       this.inputValue = parsed;
       this.outputValue = this.inputValue; 
     },
     inputFunc(func) { //演算子を押したときのイベントメソッド
       if(this.isError) {
+        console.log('error');
         return;
       }
-     
-      var val = this.calc(this.memory,func,this.inputValue); //memoryがどんどんん蓄積されていくからイコールと同様の処理になる
-      
+      if(this.inputValue == '0') { //イコール後
+        this.func = func;
+        return;
+      }
+      var val = this.calc(this.memory,func, Number(this.inputValue)); //memoryがどんどんん蓄積されていくからイコールと同様の処理になる
       if(!isNaN(val)) { //valがNaNでない場合
         this.memory = val; 
         this.func = func; // イコール時に起動させるために設定
-        this.inputValue = 0; //inputValueを初期化
-        this.outputValue = val;
+        this.inputValue = '0'; //inputValueを初期化
+        this.outputValue = val.toLocaleString(undefined, {maximumFractionDigits : 3});
       }
     },
     equal() { //＝を押した時のイベントメソッド
       if(this.isError ||  this.memory == null || this.func == null) {
         return;
       }
-      var val = this.calc(this.memory , this.func , this.inputValue);
+      var val = this.calc(this.memory , this.func , Number(this.inputValue));
       if(!isNaN(val)) {
         this.memory = val; 
         this.func = null; //初期化
-        this.inputValue = 0; //初期化
-        this.outputValue = val; 
+        this.inputValue = '0'; //初期化
+        this.outputValue = val.toLocaleString(undefined, {maximumFractionDigits: 3});
       }
     }
   }
